5e8ae580bd6ee66cf5d10c17143a178ae798efd4
Md. Masrafi Mondol <masrafisw@gmail.com>
2026-03-25 21:41:17 +0600

    about and contact

--- src/app/about/about.ts
+++ src/app/about/about.ts
@@ -1,9 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './about.html',
   styleUrl: './about.css',
 })
-export class About {}
+export class About {
+  name: string = '';
+  email: string = '';
+  message: string = '';
+
+  showToast = signal(false);
+
+  onSubmit() {
+    console.log(this.name, this.email, this.message);
+
+    // alert('Message sent!');
+
+    // reset form
+    this.name = '';
+    this.email = '';
+    this.message = '';
+
+    this.showToast.set(true);
+
+    setTimeout(() => {
+      this.showToast.set(false);
+    }, 3000);
+  }
+}
