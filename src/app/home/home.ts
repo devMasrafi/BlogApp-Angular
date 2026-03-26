75582a035d489ce8d86dd6fc6cb5327d30b96411
Md. Masrafi Mondol <masrafisw@gmail.com>
2026-03-26 13:26:53 +0600

    confim of homePage

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,21 +1,33 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core'; 
 import { BlogCard } from '../blog-card/blog-card';
 import { BlogService } from '../blog-service';
 import { RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { SlicePipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-home',
-  imports: [BlogCard, RouterLink, FormsModule],
+  imports: [BlogCard, RouterLink, FormsModule, SlicePipe],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {
+export class Home implements OnInit {
   private blogService = inject(BlogService);
 
   blogPosts = this.blogService.getPosts();
-
   searchTerm: string = '';
+
+  currentCarouselIndex: number = 0;
+
+  ngOnInit() {
+    setInterval(() => {
+      if (this.blogPosts.length > 0) {
+        
+        this.currentCarouselIndex = (this.currentCarouselIndex + 1) % this.blogPosts.length;
+      }
+    }, 4000);
+  }
 
   get filteredPosts() {
     return this.blogPosts.filter((post) =>
